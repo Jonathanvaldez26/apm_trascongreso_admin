e1b5451db21a5f1117ba8d41dc50368102ddeb35
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-06-01 16:41:32 -0500

    quitarmusa

--- backend/App/views/asistentes_all.php
+++ backend/App/views/asistentes_all.php
@@ -90,7 +90,7 @@
                         <div class="col-auto my-auto">
                             <div class="h-100">
                                 <h5 class="mb-1">
-                                    Invitados MUSA
+                                    Invitados APM
                                 </h5>
                                 <p class="mb-0 font-weight-bold text-sm">
                                 </p>
