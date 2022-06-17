a3058f1217ead41ee448570fdbb31e5f9087ea36
Your Name <jonathan.valdez@grupolahe.com>
2022-06-17 15:29:57 -0500

    Hola

--- backend/App/controllers/Asistentes.php
+++ backend/App/controllers/Asistentes.php
@@ -755,11 +755,14 @@ html;
                             <a href="/Asistentes/Detalles/{$value['clave']}" target="_blank">
                                 <h6 class="mb-0 text-sm text-move text-black">
                                     <span class="fa fa-user-md" style="font-size: 13px"></span> {$value['nombre']} {$value['segundo_nombre']} {$value['apellido_paterno']} {$value['apellido_materno']} </span> {$value['nombre_ejecutivo']} <span class="badge badge-success" style="background-color:  {$color}; color:white "><strong>{$linea} </strong></span>
-                                    <span class="badge badge-success" style="background-color: color:#ea5b9b; "><strong>Clave LAHE {$value['clave']} </strong></span>                          
+                                                         
                                     {$industria}
                                
                                 </h6>
                             </a>
+                            <div class="d-flex flex-column justify-content-center">
+                                <span class="badge badge-success" style="background-color: color:#ea5b9b; "><strong>Clave LAHE {$value['clave']} </strong></span>    
+                            </div>
                             <div class="d-flex flex-column justify-content-center">
                                 <u><a  href="mailto:{$value['email']}"><h6 class="mb-0 text-sm text-move text-black"><span class="fa fa-mail-bulk" style="font-size: 13px"></span> {$value['usuario']}</h6></a></u>
                                 <u><a target="_blank" href="https://api.whatsapp.com/send?phone=52{$value['telefono']}&text=Buen%20d%C3%ADa,%20te%20contacto%20de%20parte%20del%20Equipo%20Grupo%20LAHE%20%F0%9F%98%80" target="_blank"><p class="text-sm text-black text-move font-weight-bold text-secondary mb-0"><span class="fa fa-whatsapp" style="font-size: 13px; color:green;"></span> {$value['telefono']}</p></a></u>
