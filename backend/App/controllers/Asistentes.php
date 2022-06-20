f415801e8434b861a9b02dfd0d527474a7a35237
Your Name <jonathan.valdez@grupolahe.com>
2022-06-20 15:24:38 -0500

    Hola

--- backend/App/controllers/Asistentes.php
+++ backend/App/controllers/Asistentes.php
@@ -549,7 +549,7 @@ html;
                             <div>
                                 <span class="badge badge-success" style="background-color: #d2aaf3; color:black "><strong>REGISTRO P/PAGO</strong></span>  
                                 <span class="badge badge-success" style="background-color: #ff1d1d; color:white "><strong>NO IMPRIMIR - DIRIGIR A CAJA A PAGAR</strong></span>
-                                iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+                                
                             </div>
 html;
                         }
