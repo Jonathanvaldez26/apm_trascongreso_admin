683fd56de200ed9d9aa0038ef6f16d21e78fc13a
Your Name <jonathan.valdez@grupolahe.com>
2022-06-22 17:59:40 -0500

    Prueba

--- backend/App/controllers/Asistentes.php
+++ backend/App/controllers/Asistentes.php
@@ -738,7 +738,8 @@ html;
                         <div>
                             <div class="d-flex flex-column justify-content-center text-black">
                                 <img src="{$img_user}" class="avatar me-2" alt="image">    
-                                ***                      
+                                <br>
+                                <br>                    
                             </div>
                            
                             <div class="d-flex flex-column justify-content-center text-black">
