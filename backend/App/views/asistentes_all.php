92a485b905a30216fa64c7c66ec742ebe0def23c
Your Name <jonathan.valdez@grupolahe.com>
2022-06-22 18:08:31 -0500

    Prueba

--- backend/App/views/asistentes_all.php
+++ backend/App/views/asistentes_all.php
@@ -170,7 +170,7 @@
                                             <thead class="thead-light">
                                                 <tr>
                                                     <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Usuario</th>
-                                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Pagos Cursos Transcongresos</th>
+                                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Pagos Cursos Transcongresos - Impresión Constancia</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
