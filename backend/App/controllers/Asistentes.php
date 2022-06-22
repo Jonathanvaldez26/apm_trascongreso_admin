813deb74be46fddcf4a3aa1cb0027469f5acfead
Your Name <jonathan.valdez@grupolahe.com>
2022-06-22 17:50:07 -0500

    Prueba

--- backend/App/controllers/Asistentes.php
+++ backend/App/controllers/Asistentes.php
@@ -736,7 +736,7 @@ html;
                 <td>
                     <div class="d-flex px-1 py-1">
                         <div>
-                            <img src="{$img_user}" class="avatar me-3" alt="image">
+                            <img src="{$img_user}" class="avatar me-2" alt="image">
                             <div class="d-flex flex-column justify-content-center text-black">
                                   <a href="/RegistroAsistencia/abrirpdfGafete/{$value['clave']}/{$value['ticket_virtual']}" class="btn bg-pink btn-icon-only morado-musa-text" title="Imprimir Gafetes" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-original-title="Imprimir Gafetes" target="_blank"><i class="fas fa-print"> </i></a>     
                             </div>
