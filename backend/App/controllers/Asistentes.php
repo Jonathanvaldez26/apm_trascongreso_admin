6a60a0ca1b1f938af74b30839913b9a448789ec9
Your Name <jonathan.valdez@grupolahe.com>
2022-06-23 16:30:21 -0500

    Curs

--- backend/App/controllers/Asistentes.php
+++ backend/App/controllers/Asistentes.php
@@ -694,7 +694,7 @@ html;
                 else
                 {
                     $color_curso_pago = <<<html
-                          <span class="badge badge-danger"><strong> {$value_cursos_pagados['estatus_compra']} </strong></span>  
+                          <span class="badge badge-danger"><strong> {$value_cursos_pagados['estatus_compra']} - NO FINALIZO COMPRA</strong></span>  
 
 html;
                 }
