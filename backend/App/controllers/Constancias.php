259127bfc562ad22c98636d21091257a9b67b5f8
OscarFernandoMillanPimentel <o_fernando_m_p@ciencias.unam.mx>
2022-06-24 10:01:54 -0500

    consttranshoras

--- backend/App/controllers/Constancias.php
+++ backend/App/controllers/Constancias.php
@@ -607,9 +607,11 @@ html;
         }else{
         $pdf->Multicell(275, 10, utf8_decode($attend).' "'.utf8_decode("$nombre_constancia").'"', 0, 'C');
         }
+        //TIEMPO
         // $pdf->SetFont('Arial', 'B',10);
         // $pdf->SetXY(156, 170.5);
         // $pdf->Multicell(10, 10, utf8_decode(round($progreso['segundos']/3600)), 0, 'C');
+        //FECHA
         $pdf->SetFont('Arial', '',10);
         $pdf->SetXY(13, 175);
         $pdf->Multicell(275, 10, utf8_decode('DEL 21 AL 24 DE JUNIO DE 2022'), 0, 'C');
@@ -630,9 +632,11 @@ html;
         }else{
         $pdf->Multicell(275, 10, utf8_decode($attend).' "'.utf8_decode("$nombre_constancia").'"', 0, 'C');
         }
-        // $pdf->SetFont('Arial', 'B',10);
-        // $pdf->SetXY(156, 170.5);
-        // $pdf->Multicell(10, 10, utf8_decode(round($progreso['segundos']/3600)), 0, 'C');
+        //TIEMPO
+        $pdf->SetFont('Arial', 'B',10);
+        $pdf->SetXY(156, 170.5);
+        $pdf->Multicell(10, 10, utf8_decode('5'), 0, 'C');
+        //FECHA
         $pdf->SetFont('Arial', '',10);
         $pdf->SetXY(13, 175);
         $pdf->Multicell(275, 10, utf8_decode('DEL 21 AL 24 DE JUNIO DE 2022'), 0, 'C');
@@ -771,6 +775,11 @@ html;
             // $pdf->SetFont('Arial', 'B',10);
             // $pdf->SetXY(156, 170.5);
             // $pdf->Multicell(10, 10, utf8_decode(round($progreso['segundos']/3600)), 0, 'C');
+            //TIEMPO
+            $pdf->SetFont('Arial', 'B',10);
+            $pdf->SetXY(156, 170.5);
+            $pdf->Multicell(10, 10, utf8_decode('5'), 0, 'C');
+            //FECHA
             $pdf->SetFont('Arial', '',10);
             $pdf->SetXY(13, 175);
             $pdf->Multicell(275, 10, utf8_decode('DEL 21 AL 24 DE JUNIO DE 2022'), 0, 'C');
